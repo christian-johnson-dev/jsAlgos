b6dd17d859dc73ca91777dc5e9448c3c186d11ad
Christian Johnson <charcoaldust@mac.com>
2023-07-24 14:23:54 -0700

    initialized file

---
+++ twoSum.js
@@ -0,0 +1,16 @@
+//? Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+//*You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//*You can return the answer in any order.
+
+//==== Inputs ====//
+const array1 = [1, 5, 7, 10];
+const num1 = 15;
+
+const array2 = [4, 10, 0, 12, 15];
+const num2 = 12;
+
+const array3 = [16, 22, 78, -35];
+const num3 = 43;
+
+//
